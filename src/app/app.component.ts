818bf1e67d659721a3a1a5bd7921f858bb2a2c2d
Andrius Degutis <andriusdgt@gmail.com>
2020-04-26 20:52:51 +0300

    added base design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core'
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'The Dots'
+    title = 'The Dots';
+
+    newPointForm = this.formBuilder.group({
+        x: ['', [Validators.required, Validators.min(-10000), Validators.max(10000)]],
+        y: ['', [Validators.required, Validators.min(-10000), Validators.max(10000)]],
+    });
+
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    addPoint() {
+        console.log(this.newPointForm.value.x);
+        console.log(this.newPointForm.value.y);
+    }
+
+    deletePoint() {
+
+    }
+
+    upload() {
+
+    }
+
+    download() {
+
+    }
+
+    clearPoints() {
+
+    }
+
+    findSquares() {
+
+    }
+
+    createList() {
+
+    }
+
+    renameList() {
+
+    }
+
+    deleteList() {
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        CommonModule,
+        FormsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
